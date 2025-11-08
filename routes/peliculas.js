bf0a1ab1f28236e8679e8433a239f3cbe5c516a4
Rapa0 <153472756+Rapa0@users.noreply.github.com>
2025-11-08 09:48:33 -0400

    Update peliculas.js

--- routes/peliculas.js
+++ routes/peliculas.js
@@ -5,7 +5,7 @@ const Pelicula = require('../models/pelicula.js');
 const Comentario = require('../models/comentario.js');
 
 const TMDB_API_KEY = process.env.TMDB_API_KEY;
-const TMDB_BASE_URL = 'https.api.themoviedb.org/3';
+const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
 
 router.get('/now-playing', async (req, res) => {
   const page = req.query.page || 1;
